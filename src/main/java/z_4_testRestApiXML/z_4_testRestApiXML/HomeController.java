f099d32bafceb8fbdb89be08dde878b12f28348c
Naveen <navegoya@in.ibm.com>
2020-04-15 11:29:52 +0530

    v1

---
+++ src/main/java/z_4_testRestApiXML/z_4_testRestApiXML/HomeController.java
@@ -0,0 +1,37 @@
+/**
+ *  Copyright 2005-2016 Red Hat, Inc.
+ *
+ *  Red Hat licenses this file to you under the Apache License, version
+ *  2.0 (the "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ *  implied.  See the License for the specific language governing
+ *  permissions and limitations under the License.
+ */
+package z_4_testRestApiXML.z_4_testRestApiXML;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@PostMapping(path = "/save-cust-info")
+    public String customerInformation(@RequestBody Customer cust) {
+
+        /* You can call your DAO logic here.
+         * For time being I am printing the customer data just to show the POST call is working.
+         */
+
+        return "Customer information saved successfully ::." + cust.getCustNo() + " " + cust.getName() + " " + cust.getCountry();
+    }
+}
